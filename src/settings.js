29824baf2db77c40d2dc77066fcc08fde1fee122
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2021-01-14 18:52:04 +0100

    update contract/dapp

--- src/settings.js
+++ src/settings.js
@@ -3,11 +3,11 @@ export const githubUrl = "https://github.com/edukera/completium-dapp-ideabox"
 
 export const appTitle = "Peer to peer payment!"
 
-export const network = "carthagenet"
+export const network = "delphinet"
 
 export const appName = "AmazinCorp."
 
 export const price = "85"
 
 export const seller = "tz1ZXf37ZNfVupt5GVyrPJ76q8kbjFuD2z7R"
-export const taxCollector = "tz1Lc2qBKEWCBeDU8npG6zCeCqpmaegRi6Jg"
+export const taxCollector = "tz1MZrh8CvYkp7BfLQMcm6mg5FvL5HRZfACw"
